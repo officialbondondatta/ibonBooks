6c0c81fda698f421c287ea1dc6bdf6a3137b9306
officialbondondatta <dattabondon1320@gmail.com>
2026-09-22 22:56:09 +0600

    added banner

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -4,10 +4,10 @@ import BannerImg from "@/assets/hero_img.jpg"
 
 const Banner = () => {
     return (
-        <section className="container mx-auto">
-            <div>
-                <div className="flex flex-col space-y-3">
-                    <h2>Books to freshen up <br /> your bookshelf</h2>
+        <section className="container mx-auto lg:mt-20 mt-5 p-10 lg:p-0">
+            <div className="grid lg:grid-cols-2 grid-cols-1 mx-auto bg-slate-300 rounded-xl p-10 lg:p-20 items-center justify-center max-w-300 gap-5">
+                <div className="flex flex-col text-center gap-5">
+                    <h2 className="text-4xl font-semibold">Books to freshen up <br /> your bookshelf</h2>
                     <Link href={"/"}>
                         <button className="btn bg-green-500 hover:bg-green-700 text-white"> View The List</button>
                     </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Banner from "./components/Banner";
+
 const page = () => {
   return (
     <div>
-      HomePage
+      <Banner></Banner>
     </div>
   );
 };
