a86bd95055e6d4788e0af621bdec4d0966b760f0
officialbondondatta <dattabondon1320@gmail.com>
2026-09-23 12:49:02 +0600

    modifed read books tab

---
+++ src/app/components/ListedBookShowcase.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Book from "../types/books";
+import { CiLocationOn } from "react-icons/ci";
+import Link from "next/link";
+import { BsPeopleFill } from "react-icons/bs";
+import { GiNewspaper } from "react-icons/gi";
+
+interface IListedBooksShowcaseProps {
+    book: Book
+}
+const ListedBookShowcase = ({ book }: IListedBooksShowcaseProps) => {
+    return (
+        <div className="flex lg:grid lg:grid-cols-12 flex-col items-center justify-center gap-5 border py-5 px-3 rounded-xl border-slate-200">
+            <div className="max-w-100 lg:col-span-2 bg-slate-300 ">
+                <Image src={book.image} alt={book.bookName} width={200} height={200} className="w-[80%] py-5 mx-auto h-auto"></Image>
+            </div>
+            <div className="lg:col-span-10 space-y-3 flex flex-col">
+                <h2 className="lg:text-2xl text-xl font-semibold">{book.bookName}</h2>
+                <span>By: {book.author}</span>
+                <div className="flex lg:flex-row flex-col items-center gap-5 mb-5">
+                    <h2 className="flex items-center gap-2 mt-2">
+                        <span className="font-semibold">Tag </span>
+                        {book.tags.map((tag, index) => (
+                            <div key={index} className="text-green-500 bg-green-100 py-1 px-3 rounded-xl">
+                                <span>#{tag}</span>
+                            </div>))}
+                    </h2>
+                    <h2 className="flex items-center gap-2">
+                        <CiLocationOn className="text-2xl" /> Year of Publishing: <span className="ml-2">{book.yearOfPublishing}</span>
+                    </h2>
+                </div>
+                <div className="flex gap-5">
+                    <h2 className="flex items-center gap-2">
+                        <BsPeopleFill />
+                        Publisher: {book.publisher}
+                    </h2>
+                    <h2 className="flex items-center gap-2">
+                        <GiNewspaper className="text-xl" />
+                        Page {book.totalPages}
+                    </h2>
+                </div>
+                <hr className="w-full text-slate-300" />
+                <div className="flex flex-col lg:flex-row items-center gap-5">
+                    <h2 className="bg-blue-200 text-blue-500 px-3 py-1 rounded-full">Category: {book.category}</h2>
+                    <h2 className="bg-orange-100 text-orange-500 px-3 py-1 rounded-full">Rating: {book.rating}</h2>
+                    <Link href={`books/${book.bookId}`}>
+                        <button className="bg-green-500 px-4 py-1 text-white rounded-full font-semibold">View Details</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ListedBookShowcase;
--- src/app/listed-books/page.tsx
+++ src/app/listed-books/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { BookContext } from "@/context/BookContext";
 import { useContext } from "react";
+import ListedBookShowcase from "../components/ListedBookShowcase";
 
 const ListedBooksPage = () => {
     const BookContextValue = useContext(BookContext)
@@ -12,10 +13,29 @@ const ListedBooksPage = () => {
     const { readBooks, wishlist } = BookContextValue
     return (
         <section className="p-5 lg:p-0 mt-5">
-            <div className="container mx-auto bg-slate-100 space-y-4 p-5">
-                <h1 className="text-center my-5 text-4xl font-bold text-slate-600">Listed Books</h1>
-                <h1>readBooks: {readBooks.length}</h1>
-                <h1>wishList: {wishlist.length}</h1>
+            <div className="container mx-auto space-y-4 p-5">
+                <h1 className="text-center my-5 text-4xl font-bold bg-slate-100 py-8 rounded-xl text-slate-600">Listed Books</h1>
+                <div>
+                    <div className="tabs tabs-lift">
+                        <input type="radio" name="my_tabs_1" className="tab" aria-label="Read Books" defaultChecked />
+                        <div className="tab-content  bg-base-100 border-base-300 p-6 ">
+                            <div className="grid grid-cols-1 gap-5">
+                                {
+                                    readBooks.length === 0 ? "No Books added yet" :
+                                        readBooks.map(book => (
+                                            <ListedBookShowcase book={book} key={book.bookId}>
+
+                                            </ListedBookShowcase>
+                                        ))
+                                }
+                            </div>
+                        </div>
+
+                        <input type="radio" name="my_tabs_1" className="tab" aria-label="Wishlist Books" />
+                        <div className="tab-content bg-base-100 border-base-300 p-6">Tab content 2</div>
+
+                    </div>
+                </div>
             </div>
         </section>
     );
